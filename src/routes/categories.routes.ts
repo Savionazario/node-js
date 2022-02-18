2b8efa561fe4e4fb8e7e7c998e7adc320764f3bf
Savio Nazario <savio.nazario88@gmail.com>
2022-02-17 22:55:03 -0300

    Creating Category model class and update .eslint and categories.routes.ts files

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,35 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
+import { Category } from "../model/category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+// Criando array de nome "categories" e de tipo "Category[]" que é uma classe
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  // criando objeto
-  const category = {
+  // criando objeto, porém dessa forma o constructor não é chamado
+  // const category: Category = {
+  //   name,
+  //   description,
+  //   created_at: new Date(),
+  // };
+
+  // Criando objeto
+  const category = new Category();
+
+  // É passado para o assign dois parâmetros, o primeiro é um objeto(que nesse caso é "category")
+  // e o segundo são os atributos que serão jogados/preenchidos para dentro do objeto
+  Object.assign(category, {
     name,
     description,
-    id: uuidV4(),
-  };
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
